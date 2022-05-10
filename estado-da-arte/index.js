5f5ec0b02a91524b221c5d483c80ee1d673dbe31
Matheus N Ramos <mnishidaramos@gmail.com>
2022-05-10 19:17:50 -0300

    Versao inicial de Estado da Arte

---
+++ estado-da-arte/index.js
@@ -0,0 +1,8 @@
+function changeContent(id) {
+    // console.log(document.getElementsByClassName("conteudo-texto")[0].innerHTML);
+    // document.getElementsByClassName("conteudo-texto").array.forEach(element => {
+
+    // });
+    // document.getElementsById("conteudo-texto1").style.display = "none";
+    // document.getElementById(id).style.display = "true";
+}
