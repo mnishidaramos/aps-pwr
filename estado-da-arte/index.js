37befac5f5067bbcbbbc9b580fcc0f4a4f0a4571
Matheus N Ramos <mnishidaramos@gmail.com>
2022-05-12 09:53:51 -0300

    Estado da arte - Segunda versao

--- estado-da-arte/index.js
+++ estado-da-arte/index.js
@@ -1,12 +1,25 @@
 //Função que altera qual conteúdo está sendo exibido na coluna da direita
-function changeContent(id) {
+function changeContent(e, id) {
+    //Mostra o elemento com o id passado como parâmetro
     var elements = document.getElementsByClassName("conteudo-texto");
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
         if (element.id === id) {
             element.style.display = "block";
+
         } else {
             element.style.display = "none";
         }
     };
+    //Adiciona uma borda ao card clicado
+    var cards = document.getElementsByClassName("card");
+    for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        //Se o card clicado for o card atual
+        if (e.target.parentNode.parentNode === card) {
+            card.classList.add("border-primary");
+        } else {//Do contrário
+            card.classList.remove("border-primary");
+        }
+    }
 }
