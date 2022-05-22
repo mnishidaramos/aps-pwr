921e6b7bffebc87f4e4cc73829c70f78831b9de0
André Marinzeck <marinzeck01@gmail.com>
2022-05-22 05:46:16 -0300

    Deixando a parte industrial responsiva

--- pagina_iot_industrial/iot_industrial_js/iot_industrial.js
+++ pagina_iot_industrial/iot_industrial_js/iot_industrial.js
@@ -8,10 +8,10 @@ function changeContent(classes, botao) {
     texto.style.display = "block";
 
     botaoInativo.forEach(function(element) {
-        element.style.borderTop = "5px solid #00000000";
+        element.style.borderBottom = "5px solid #00000000";
         element.style.color = "#F2F2F2";
     })
-    botaoAtivo.style.borderTop = "4px solid #F2C12E";
+    botaoAtivo.style.borderBottom = "4px solid #F2C12E";
     botaoAtivo.style.color = "#F2C12E",
     
     
